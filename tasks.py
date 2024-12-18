4e65779dfe0258e9411a7238bbcef8834db3c9a3
Justin Mayer <entroP@gmail.com>
2024-12-18 11:45:06 +0100

    chore: Update Invoke tasks for uv + Ruff

--- tasks.py
+++ tasks.py
@@ -1,71 +1,93 @@
+from inspect import cleandoc
+import logging
 import os
 from pathlib import Path
 from shutil import which
 
 from invoke import task
 
+logger = logging.getLogger(__name__)
+
 PKG_NAME = "typogrify"
 PKG_PATH = Path(f"{PKG_NAME}")
+
 ACTIVE_VENV = os.environ.get("VIRTUAL_ENV", None)
 VENV_HOME = Path(os.environ.get("WORKON_HOME", "~/.local/share/virtualenvs"))
-VENV_PATH = Path(ACTIVE_VENV) if ACTIVE_VENV else (VENV_HOME / PKG_NAME)
+VENV_PATH = Path(ACTIVE_VENV) if ACTIVE_VENV else (VENV_HOME.expanduser() / PKG_NAME)
 VENV = str(VENV_PATH.expanduser())
+BIN_DIR = "bin" if os.name != "nt" else "Scripts"
+VENV_BIN = Path(VENV) / Path(BIN_DIR)
 
-TOOLS = ["poetry", "pre-commit"]
-POETRY = which("poetry") if which("poetry") else (VENV / Path("bin") / "poetry")
-PRECOMMIT = (
-    which("pre-commit") if which("pre-commit") else (VENV / Path("bin") / "pre-commit")
-)
+CMD_PREFIX = f"{VENV_BIN}/" if ACTIVE_VENV else "uv run "
+PRECOMMIT = which("pre-commit") if which("pre-commit") else "uvx run pre-commit"
+PTY = os.name != "nt"
 
 
 @task
-def tests(c):
-    """Run the test suite"""
+def tests(c, deprecations=False):
+    """Run the test suite, optionally with `--deprecations`."""
+    deprecations_flag = "" if deprecations else "-W ignore::DeprecationWarning"
     c.run(
-        f"{VENV}/bin/pytest -v --doctest-modules typogrify/filters.py typogrify/packages/titlecase/tests.py",
+        f"{CMD_PREFIX}pytest {deprecations_flag} --doctest-modules typogrify/filters.py typogrify/packages/titlecase/tests.py",
         pty=True,
     )
 
 
 @task
-def black(c, check=False, diff=False):
-    """Run Black auto-formatter, optionally with --check or --diff"""
+def format(c, check=False, diff=False):
+    """Run Ruff's auto-formatter, optionally with `--check` or `--diff`."""
     check_flag, diff_flag = "", ""
     if check:
         check_flag = "--check"
     if diff:
         diff_flag = "--diff"
-    c.run(f"{VENV}/bin/black {check_flag} {diff_flag} {PKG_PATH} tasks.py")
+    c.run(
+        f"{CMD_PREFIX}ruff format {check_flag} {diff_flag} {PKG_PATH} tasks.py", pty=PTY
+    )
 
 
 @task
-def pycodestyle(c):
-    c.run(f"{VENV}/bin/pycodestyle {PKG_PATH} tasks.py")
+def ruff(c, concise=False, fix=False, diff=False):
+    """Run Ruff to ensure code meets project standards."""
+    concise_flag, fix_flag, diff_flag = "", "", ""
+    if concise:
+        concise_flag = "--output-format=concise"
+    if fix:
+        fix_flag = "--fix"
+    if diff:
+        diff_flag = "--diff"
+    c.run(f"{CMD_PREFIX}ruff check {concise_flag} {diff_flag} {fix_flag} .", pty=PTY)
 
 
 @task
-def lint(c):
-    black(c, check=True)
-    pycodestyle(c)
-
-
-@task
-def tools(c):
-    """Install tools in the virtual environment if not already on PATH"""
-    for tool in TOOLS:
-        if not which(tool):
-            c.run(f"{VENV}/bin/pip install {tool}")
+def lint(c, concise=False, fix=False, diff=False):
+    """Check code style via linting tools."""
+    ruff(c, concise=concise, fix=fix, diff=diff)
+    format(c, check=(not fix), diff=diff)
 
 
 @task
 def precommit(c):
-    """Install pre-commit hooks to .git/hooks/pre-commit"""
+    """Install pre-commit hooks to .git/hooks/pre-commit."""
+    logger.info("** Installing pre-commit hooks **")
     c.run(f"{PRECOMMIT} install")
 
 
 @task
 def setup(c):
-    c.run(f"{VENV}/bin/pip install -U pip")
-    tools(c)
-    c.run(f"{POETRY} install")
-    precommit(c)
+    """Set up the development environment."""
+    if which("uv") or ACTIVE_VENV:
+        c.run("uv sync --all-groups", pty=PTY)
+        precommit(c)
+        logger.info("\nDevelopment environment should now be set up and ready!\n")
+    else:
+        error_message = """
+            uv is not installed, and there is no active virtual environment available.
+            You can either manually create and activate a virtual environment, or you can
+            install uv via:
+
+            curl -LsSf https://astral.sh/uv/install.sh | sh
+
+            Once you have taken one of the above two steps, run `invoke setup` again.
+            """  # noqa: E501
+        raise SystemExit(cleandoc(error_message))
